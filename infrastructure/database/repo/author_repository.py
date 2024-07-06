6e799c259c2868b2cdcc4b7d3b0d94e1034ca7f0
Soldatov Serhii <soldatov.own@gmail.com>
2024-07-06 21:03:57 +0400

    add sql to get authors by name

--- infrastructure/database/repo/author_repository.py
+++ infrastructure/database/repo/author_repository.py
@@ -1,9 +1,8 @@
-
-from sqlalchemy import select
+from sqlalchemy import and_, exists, or_, select
 
 from infrastructure.database.models import AuthorDescriptionModel, AuthorModel
 from infrastructure.database.repo.base import BaseRepo
-from infrastructure.dtos.author_dtos import AuthorInfoDTO
+from infrastructure.dtos.author_dtos import AuthorBaseDTO, AuthorInfoDTO
 
 
 class AuthorRepo(BaseRepo):
@@ -22,5 +21,51 @@ class AuthorRepo(BaseRepo):
             AuthorInfoDTO(book_id=author.book_id, pos=author.pos, **description.__dict__)
             for author, description in authors
         ]
+        return authors_info
 
+    async def get_authors_by_name(self, name: str) -> list[AuthorInfoDTO] | None:
+        """
+        Retrieves a list of authors based on a provided name, ensuring each author has associated books.
+
+        This function processes the input name to handle scenarios where it might consist of either
+        a full name (first and last), or just a single name part.
+        """
+        name_parts = name.split()
+        if not name_parts:
+            return []
+
+        book_exists_subquery = (
+            select(AuthorModel.author_id)
+            .where(AuthorModel.author_id == AuthorDescriptionModel.author_id)
+            .exists()
+        )
+
+        conditions = []
+        if len(name_parts) == 2:
+            first_name, last_name = name_parts
+            conditions.append(AuthorDescriptionModel.first_name.ilike(f'{first_name}%'))
+            conditions.append(AuthorDescriptionModel.last_name.ilike(f'{last_name}%'))
+        else:
+            name = name_parts[0]
+            conditions.append(
+                or_(
+                    AuthorDescriptionModel.first_name.ilike(f'{name}%'),
+                    AuthorDescriptionModel.last_name.ilike(f'{name}%')
+                )
+            )
+        conditions.append(exists(book_exists_subquery))
+
+        query = (
+            select(AuthorDescriptionModel)
+            .join(AuthorModel, AuthorModel.author_id == AuthorDescriptionModel.author_id)
+            .where(and_(*conditions))
+            .group_by(AuthorDescriptionModel.author_id)
+        )
+
+        result = await self.session.execute(query)
+        authors = result.scalars().all()
+        if not authors:
+            return []
+
+        authors_info = [AuthorBaseDTO(**description.__dict__) for description in authors]
         return authors_info
